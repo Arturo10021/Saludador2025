763b6029131fe7b72854717821e85f516bafd125
Arturo10021 <arturo.guzman@ucb.edu.bo>
2024-08-20 00:44:08 -0400

    error correction

--- src/saludador.js
+++ src/saludador.js
@@ -3,15 +3,16 @@ export class botSaludos {
     let saludo="";
     let tipoSaludo = ["Hola", "Buenos días", "Buenas tardes", "Buenas noches"];
     let add =0;
-        if(hora >= 0 && hora < 12){
+        if (idioma === "en") {
+            tipoSaludo = ["Hello","Good morning", "Good afternoon", "Good evening"];
+        }
+        if (hora > 6 && hora < 12) {
             add = 1;
-        }
-        else if(hora >= 12 && hora < 19){
+        } else if (hora >= 12 && hora < 20) {
             add = 2;
-        }
-        else if(hora >= 19 && hora < 24){
+        } else  if (hora >= 20 || hora < 6) {
             add = 3;
-        }else{
+        } else {
             add = 0;
         }
         saludo = tipoSaludo[add] + " ";
