75f63b13fbe3bd008f457af91485e9f6162b624b
Arturo10021 <arturo.guzman@ucb.edu.bo>
2024-08-20 01:31:34 -0400

    add idiomas

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,17 @@
 import {botSaludos} from "./saludador.js";
 
-const form = document.querySelector("#botSaludos-form");
-const div = document.querySelector("#resultado-div");
-const horaActual = new Date().getHours();
-const nombre = document.querySelector("#nombre");
-const genero = document.querySelector("#genero");
-const edad = document.querySelector("#edad");
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const botsaludos = new botSaludos();
-  console.log(horaActual.value);
-  div.innerHTML = "<p>" + botsaludos.saludar(nombre.value,horaActual,genero.value, edad.value) + "</p>";
-});
+    const nombre = document.querySelector("#nombre");
+    const form = document.querySelector("#botSaludos-form");
+    const horaActual = new Date().getHours();
+    const genero = document.querySelector("#genero");
+    const edad = document.querySelector("#edad");
+    const idioma = document.querySelector("#idioma");
+    const div = document.querySelector("#resultado-div");
+    
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+        const botsaludos = new botSaludos();
+        console.log(horaActual.value);
+        div.innerHTML = "<p>"+botsaludos.saludar(nombre.value,horaActual,genero.value, edad.value, idioma.value)+"</p>";
+    }
+);
--- src/saludador.js
+++ src/saludador.js
@@ -1,8 +1,8 @@
 export class botSaludos {  
-    saludar(nombre,hora,genero,edad) {
-    let saludo="";
-    let tipoSaludo = ["Hola", "Buenos días", "Buenas tardes", "Buenas noches"];
-    let add =0;
+    saludar(nombre, hora, genero, edad, idioma) {
+      let saludo = "";
+      let tipoSaludo = ["Hola","Buenos dias", "Buenas tardes", "Buenas noches"];
+      let add = 0;
         if (idioma === "en") {
             tipoSaludo = ["Hello","Good morning", "Good afternoon", "Good evening"];
         }
@@ -16,14 +16,31 @@ export class botSaludos {
             add = 0;
         }
         saludo = tipoSaludo[add] + " ";
-        if ((genero === "M" || genero === "m") && edad >=30) {
+
+        if ((genero === "M" || genero === "m") && edad >=30 && idioma != "en") {
             saludo += "Sr. ";
-        }else if ((genero === "F" || genero === "f") && edad >=30) {
+        }else if ((genero === "F" || genero === "f") && edad >=30 && idioma != "en") {
             saludo += "Sra. ";
+        }else if ((genero === "M" || genero === "m") && edad <30 && idioma != "en") {
+            saludo += "Joven ";
+        }else if ((genero === "F" || genero === "f") && edad <30 && idioma != "en") {
+            saludo += "Jovencita ";
+        }else if ((genero === "M" || genero === "m") && edad >=30 && idioma === "en") {
+            saludo += "Mr. ";
         }
-        saludo+= nombre;
-        console.log(nombre);
-    return saludo;
+        else if ((genero === "F" || genero === "f") && edad >=30 && idioma === "en") {
+            saludo += "Mrs. ";
+        }
+        else if ((genero === "M" || genero === "m") && edad <30 && idioma === "en") {
+            saludo += "Young ";
+        }
+        else if ((genero === "F" || genero === "f") && edad <30 && idioma === "en") {
+            saludo += "Miss ";
+        } 
+
+      saludo+= nombre;
+      console.log(nombre);
+      return saludo;
     }
 }
 export default botSaludos;
