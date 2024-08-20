4fb92056d6264075ad72ccdbf6c3404b8520e23d
Arturo10021 <arturo.guzman@ucb.edu.bo>
2024-08-20 00:39:00 -0400

    add more types of saludo

--- src/saludador.js
+++ src/saludador.js
@@ -1,9 +1,20 @@
 export class botSaludos {  
     saludar() {
     let saludo="";
-    let tipoSaludo = ["Hola"];
+    let tipoSaludo = ["Hola", "Buenos días", "Buenas tardes", "Buenas noches"];
     let add =0;
-    saludo = tipoSaludo[add] + " ";
+        if(hora >= 0 && hora < 12){
+            add = 1;
+        }
+        else if(hora >= 12 && hora < 19){
+            add = 2;
+        }
+        else if(hora >= 19 && hora < 24){
+            add = 3;
+        }else{
+            add = 0;
+        }
+        saludo = tipoSaludo[add] + " ";
     return saludo;
     }
 }
